1cec763fe87ae3e3b9fb7251296ad3fc89e937e6
g0ooo0gle <mail.keisuke.nakamura01234@gmail.com>
2018-04-29 20:32:15 +0900

    初版

---
+++ src/common/MainPanel.java
@@ -0,0 +1,85 @@
+package common;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class MainPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public MainPanel() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+
+		JLabel NameLabel = new JLabel("TestPanel");
+		GridBagConstraints gbc_NameLabel = new GridBagConstraints();
+		gbc_NameLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_NameLabel.gridx = 0;
+		gbc_NameLabel.gridy = 0;
+		add(NameLabel, gbc_NameLabel);
+
+		JButton btnOn = new JButton("ON");
+		btnOn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				NameLabel.setText("ON");
+			}
+		});
+		GridBagConstraints gbc_btnOn = new GridBagConstraints();
+		gbc_btnOn.insets = new Insets(0, 0, 5, 5);
+		gbc_btnOn.gridx = 0;
+		gbc_btnOn.gridy = 1;
+		add(btnOn, gbc_btnOn);
+
+		JButton btnOff = new JButton("OFF");
+		btnOff.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				NameLabel.setText("OFF");
+			}
+		});
+		GridBagConstraints gbc_btnOff = new GridBagConstraints();
+		gbc_btnOff.insets = new Insets(0, 0, 5, 0);
+		gbc_btnOff.gridx = 1;
+		gbc_btnOff.gridy = 1;
+		add(btnOff, gbc_btnOff);
+
+		JButton btnOn_1 = new JButton("ON");
+		GridBagConstraints gbc_btnOn_1 = new GridBagConstraints();
+		gbc_btnOn_1.insets = new Insets(0, 0, 5, 5);
+		gbc_btnOn_1.gridx = 0;
+		gbc_btnOn_1.gridy = 2;
+		add(btnOn_1, gbc_btnOn_1);
+
+		JButton btnOff_1 = new JButton("OFF");
+		GridBagConstraints gbc_btnOff_1 = new GridBagConstraints();
+		gbc_btnOff_1.insets = new Insets(0, 0, 5, 0);
+		gbc_btnOff_1.gridx = 1;
+		gbc_btnOff_1.gridy = 2;
+		add(btnOff_1, gbc_btnOff_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 0, 5);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 3;
+		add(scrollPane, gbc_scrollPane);
+		
+		JPanel panel = new JPanel();
+		scrollPane.setColumnHeaderView(panel);
+
+	}
+
+}
---
+++ src/gbRoomControl/GBMain.java
@@ -0,0 +1,53 @@
+package gbRoomControl;
+
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+
+import common.MainPanel;
+
+public class GBMain extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GBMain frame = new GBMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GBMain() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		MainPanel mainPanel = new  MainPanel();
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		scrollPane.setViewportView(mainPanel);
+		
+		contentPane.add(scrollPane);
+	}
+
+}
