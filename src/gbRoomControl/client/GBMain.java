346d66964ebd2a64917f1727070be3ad20b78784
g0ooo0gle <mail.keisuke.nakamura01234@gmail.com>
2018-05-27 22:41:30 +0900

    読み込み機能

--- src/gbRoomControl/client/GBMain.java
+++ src/gbRoomControl/client/GBMain.java
@@ -9,7 +9,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
-import gbRoomControl.client.common.MainPanel;
+import gbRoomControl.client.panel.MainPanel;
 
 public class GBMain extends JFrame {
 
@@ -41,13 +41,13 @@ public class GBMain extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
-		
-		MainPanel mainPanel = new  MainPanel();
+
+		MainPanel mainPanel = new MainPanel();
 		GridBagLayout gridBagLayout = (GridBagLayout) mainPanel.getLayout();
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0};
-		
+		gridBagLayout.columnWeights = new double[] { 0.0, 1.0 };
+
 		JScrollPane scrollPane = new JScrollPane();
-		
+
 		scrollPane.setViewportView(mainPanel);
 		contentPane.add(scrollPane);
 	}
--- src/gbRoomControl/client/panel/MainPanel.java
+++ src/gbRoomControl/client/panel/MainPanel.java
@@ -1,4 +1,4 @@
-package gbRoomControl.client.common;
+package gbRoomControl.client.panel;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
